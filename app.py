9629535b9c50ae2015457ba89737fe1d46a50558
shankjbs571 <shankjbs571@gmail.com>
2024-07-29 14:31:10 +0530

    add flask app

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template_string
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template_string('''
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Flask App</title>
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">FlaskApp</a>
+        </nav>
+        <div class="container mt-5">
+            <div class="jumbotron">
+                <h1 class="display-4">Hello, World!</h1>
+                <p class="lead">This is a simple Flask application serving a basic HTML page with Bootstrap styling.</p>
+                <hr class="my-4">
+                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+            </div>
+        </div>
+    </body>
+    </html>
+    ''')
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- main.py
+++
@@ -1,1 +0,0 @@
-print("dsdshello  CICD testing...!")
