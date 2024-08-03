ab28f5978f28f88d3d75ea3e08affccd374475d0
shankjbs571 <shankjbs571@gmail.com>
2024-08-03 19:42:46 +0530

    update app.log dir

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def home():
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Flask App main</title>
+        <title>Flask App main 2</title>
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
     </head>
     <body>
